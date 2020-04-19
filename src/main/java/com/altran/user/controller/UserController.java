70a6ff4e7f1b026cf256b84293b7d6588b370350
4013707 <amvabhijith@gmail.com>
2020-04-19 23:34:47 +0530

    usercontroller unit testing

--- src/main/java/com/altran/user/controller/UserController.java
+++ src/main/java/com/altran/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.altran.user.controller;
 import com.altran.user.model.User;
 import com.altran.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +19,7 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/registration")
-    public ResponseEntity<User> getEmployee(@ModelAttribute User user) {
+    public ResponseEntity<User> registration(@RequestBody User user) {
         return ResponseEntity.ok(userService.registration(user));
     }
 
--- src/test/java/com/altran/user/UserApplicationTests.java
+++ src/test/java/com/altran/user/UserApplicationTests.java
@@ -1,13 +1,22 @@
 package com.altran.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.altran.user.controller.UserController;
 
 @SpringBootTest
 class UserApplicationTests {
 
+	@Autowired
+	private UserController controller;
+
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
